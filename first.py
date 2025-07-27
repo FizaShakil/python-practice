bdad6bc1f916225c8fb825cb729e79f9d6507105
FizaShakil <fizashakil25@gmail.com>
2025-07-27 14:22:32 +0500

    Fisrt program

---
+++ first.py
@@ -0,0 +1,15 @@
+# concatenation
+f_name = "John"
+l_name = "Doe"
+print(f_name + " " + l_name )
+
+# Ha 3-times 
+
+hahaha = "Ha" * 3
+print(hahaha)
+
+#list
+list_1 = [10, 20, 30, 40 ,50]
+
+for l in list_1:
+    print(l)
